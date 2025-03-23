9cd7543e4c57b70a95308ea3f660d048a66ff052
ebubecodes <123279755+ebubecodes@users.noreply.github.com>
2025-03-23 19:39:42 +0100

    comment

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     ensureAuth: function (req, res, next) {
-      if (req.isAuthenticated()) {
+      if (req.isAuthenticated()) {  // part of passport.js "strategy"
         return next()
       } else {
         res.redirect('/')
